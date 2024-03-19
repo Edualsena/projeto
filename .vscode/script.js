253262b1fa53fb8b7ea4ee2da36d289f4197715f
“Edualsena” <eduedualsena12390@gmail.com>
2024-03-19 00:27:19 -0300

    atulizaçao

--- .vscode/script.js
+++ .vscode/script.js
@@ -3,13 +3,13 @@ function toggleMode() {
   html.classList.toggle("light")
 
   // pegar a tag img
-  const img = document.queryselecto("#profile img")
+  const img = document.querySelector("#profile img")
   // substituir a imagem
   if (html.classList.contains("light")) {
     // se tiver light mode, adicionar a imagem light
-    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("src", "./assets/perfil-light.jpg")
   } else {
     //se tiver sem light mode , mater a imagem normal
-    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("src", "./assets/perfil-dark.jpg")
   }
 }
